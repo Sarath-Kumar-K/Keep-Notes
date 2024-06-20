120b593121fd6d46de4086cf2ea105b038ec1f1a
SARATH KUMAR K <sarathkrishnan2332@gmail.com>
2024-06-21 04:17:36 +0530

    Added pre installation for deployment

--- api/server.js
+++ api/server.js
@@ -22,3 +22,19 @@ mongoose.connect(process.env.MONGO).then(()=>{
 app.use(express.json());
 
 app.use('/api/note',noteRoutes);
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../client/dist/index.html'));
+});
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+});
